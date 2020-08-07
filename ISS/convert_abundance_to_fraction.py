c73f5150f2355f6badceceb6c7c059a52d3264f6
Ana Han <anahan2011@gmail.com>
2020-08-07 02:11:42 -0400

    Finalize abundancy as fraction

--- ISS/convert_abundance_to_fraction.py
+++ ISS/convert_abundance_to_fraction.py
@@ -5,5 +5,5 @@ import os
 abundance_percentage_PATH="refseq_genomeID_abundancies.txt"
 
 abundance_fraction = pd.read_csv(abundance_percentage_PATH, delim_whitespace=True)
-abundance_fraction.insert(0, 'abundance_percentage', abundance_fraction['fixed_abundances']/100)
-abundance_fraction.iloc[:,0].to_csv(abundance_percentage_PATH.replace("abundancies", "abundancies_percentage"), index=False, sep="\t")
+abundance_fraction.insert(1, 'abundance_fraction', abundance_fraction['fixed_abundances']/100)
+abundance_fraction.iloc[:,[0,1]].to_csv(abundance_percentage_PATH.replace("abundancies", "abundancies_fraction"), index=False, sep="\t")
